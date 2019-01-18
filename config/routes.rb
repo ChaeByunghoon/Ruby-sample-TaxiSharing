bb00127c54b42d05488e61dd0ab154c64fa66f40
ChaeByunghoon <cbh1203@naver.com>
2019-01-18 16:53:04 +0900

    initial Setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+
+  get 'login/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'login#index'
 end
